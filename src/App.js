1f357acd02ab6655bb5877a7be9d383e17abdb17
carlo-santos-oc <carlosantos@student.olympic.edu>
2023-06-05 23:45:22 -0700

    typography change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  Typography
-} from '@mui/material';
-@mui / material'
+import { Typography } from '@mui/material';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
