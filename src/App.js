794856e3a4803d5d6e4b0a24877a2b2357a13f17
carlo-santos-oc <carlosantos@student.olympic.edu>
2023-06-13 14:19:02 -0700

    added header and caption to images

--- src/App.js
+++ src/App.js
@@ -9,7 +9,17 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { ClassNames } from '@emotion/react';
 
-const cards = [{ id: 1, source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3081.jpg' }, { id: 2, source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3104.jpg' }, { id: 3, source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3149.jpg' }, { id: 4, source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3166.jpg' }, { id: 5, source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3219.jpg' }, { id: 6, source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3291.jpg' }, { id: 7, source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3331.jpg' }, { id: 9, source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3351.jpg' }, { id: 9, source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3527.jpg'}];
+const cards = [
+  { id: 1, header: 'Coming home', caption: 'First car ride back to Seattle.', source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3081.jpg' },
+  { id: 2, header: 'Exploring', caption: 'Rayna finding her spots in the apartment.', source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3104.jpg' },
+  { id: 3, header: 'New bed', caption: 'Enjoying her new bed.', source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3149.jpg' },
+  { id: 4, header: 'Couch sitting', caption: 'Watching the tv make noises.', source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3166.jpg' },
+  { id: 5, header: 'Vet visit', caption: 'First time at the vet.', source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3219.jpg' },
+  { id: 6, header: 'Another bed', caption: 'Enjoing yet another new bed.', source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3291.jpg' },
+  { id: 7, header: 'Hot weather', caption: 'Napping on the cool floor.', source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3331.jpg' },
+  { id: 9, header: 'Tuckered out', caption: 'Nap after playing.', source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3351.jpg' },
+  { id: 9, header: 'Bath time', caption: 'Not enjoying her first bath in the tub.', source: 'https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3527.jpg' }
+];
 
 const App = () => {
   return (
@@ -21,7 +31,7 @@ const App = () => {
           margin-right: 20px;
         `} />
           <Typography variant="h6">
-            Photo Album
+            Carlo Santos IS491 Jenkins Project
           </Typography>
         </Toolbar>
       </AppBar>
@@ -80,9 +90,9 @@ const App = () => {
           flex-grow: 1;
         `}>
                     <Typography gutterBottom variant="h5">
-                      Heading
+                      {val.header}
                     </Typography>
-                    <Typography>This is a media card. You can use this section to describe the content</Typography>
+                    <Typography>{val.caption}</Typography>
                   </CardContent>
                   <CardActions>
                     <Button size="sm" color="primary">View</Button>
@@ -91,53 +101,22 @@ const App = () => {
                 </Card>
               </Grid>
             ))}
-            
-          </Grid>
-        </Container>
-        {/* <Container maxWidth="md" className={css`
-          padding: 20px 0;
-        `}>
-          <Grid container spacing={4}>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card className={css`
-          height: 100%;
-          display: flex;
-          flex-direction: column;
-        `}>
-                  <CardMedia
-                  image='https://olympiccollegefiles.s3.us-west-2.amazonaws.com/IMG-3527.jpg'
-                    title="Rayna image" className={css`
-          padding-top: 66.67%;
-        `} />
-                  <CardContent className={css`
-          flex-grow: 1;
-        `}>
-                    <Typography gutterBottom variant="h5">
-                      Heading
-                    </Typography>
-                    <Typography>This is a media card. You can use this section to describe the content</Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="sm" color="primary">View</Button>
-                    <Button size="sm" color="primary">Edit</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
 
           </Grid>
-        </Container> */}
+        </Container>
+
       </main>
       <footer className={css`
           padding: 50px 0;
           background color: #FFF;
         `}>
-          <Typography variant="h6" align="center" gutterBottom>
-            Footer
-          </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary">
-          Something here to give the footer a purpose
+        <Typography variant="h6" align="center" gutterBottom>
+          Created by: Carlo Santos
         </Typography>
-        </footer>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          IS491 Jenkins project.
+        </Typography>
+      </footer>
     </>
   )
 }
